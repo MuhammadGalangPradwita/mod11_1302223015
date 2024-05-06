64ac832d65e6f45d58fddcefa2844d20b2942b14
braacellettt <104008593+MuhammadGalangPradwita@users.noreply.github.com>
2024-05-06 19:50:08 +0700

    Add files via upload

---
+++ modul11_1302223015/ConsoleAppMatematika/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProgramMain;
+using MatematikaLibraries;
+
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Matematika.FPB(60, 45));
+            Console.WriteLine(Matematika.KPK(12, 8));
+            Console.WriteLine(Matematika.Turunan([1, 4, -12, 9]));
+            Console.WriteLine(Matematika.Integral([4,6,-12,9]));
+        }
+    }
+
